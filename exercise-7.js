f77277ddbc777fb42d9c7436772327422d59f1c4
Chiara Arrivas <ritarrivas96@gmail.com>
2022-10-31 12:00:02 +0100

    Getter e Setter

---
+++ exercise-7.js
@@ -0,0 +1,48 @@
+class Person {
+
+  firstName = "";
+  lastName = "";
+  age = 0;
+
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    if (typeof (age) === "number") {
+      this.age = age;
+    } else {
+      this.age = Number(age)
+    }
+  }
+  set firstName(newFirstName) {
+    this.firstName = newFirstName;
+  }
+  get firstName() {
+    return this.firstName;
+  }
+
+  set lastName(newlastName) {
+    this.lastName = newlastName;
+  }
+  get lastName() {
+    return this.lastName
+  }
+
+  set age(newAge) {
+    this.age = newAge;
+  }
+  get age() {
+    return this.age
+  }
+
+  get fullName() {
+    return this.firstName + " " + this.lastName
+  }
+
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
